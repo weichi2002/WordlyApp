e50b9704a9cad1ca90fa5c9f2d0d96cf6b4e0c63
weichi2002 <wchou@fandm.edu>
2023-03-02 00:33:24 -0500

    added the logo icon and designed some arts in the resoruce folder

---
+++ app/src/main/java/edu/fandm/wchou/myapplication/SplashPage.java
@@ -0,0 +1,14 @@
+package edu.fandm.wchou.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_page);
+    }
+}
