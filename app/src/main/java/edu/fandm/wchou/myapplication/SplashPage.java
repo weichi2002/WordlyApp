6d335af63650cea48380263acbde19da6c4a3f35
Brennan Allen <brennandoubt@gmail.com>
2023-03-02 17:06:15 -0500

    Made splash screen class the launcher for app

--- app/src/main/java/edu/fandm/wchou/myapplication/SplashPage.java
+++ app/src/main/java/edu/fandm/wchou/myapplication/SplashPage.java
@@ -1,8 +1,12 @@
 package edu.fandm.wchou.myapplication;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashPage extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class SplashPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_page);
+
+        // hide button bar
+        View text_view = findViewById(R.id.splash_screen_text_tv);
+        View rootView = text_view.getRootView();
+        rootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+        // hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+
+        // Switches to another activity after 1.375s
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        }, 1375);
     }
 }
